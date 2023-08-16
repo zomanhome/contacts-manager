8dcfdc733a426fb541afd40d4fd4748a35b2bb03
Andriian Sokolenko <asokolenko@BaDM.ua>
2023-08-16 18:47:30 +0300

    wds

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import reactDOM from "react-dom"
+import React from "react"
+
+const App = () => {
+
+  return (
+    <div>
+      <h1>Contacts manager</h1>
+    </div>
+  )
+}
+
+reactDOM.render(<App />, document.getElementById("app"))
