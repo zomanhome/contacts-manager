19b750065914a23a506ed2bc9cab704f0f7bd031
Andriian Sokolenko <asokolenko@BaDM.ua>
2023-08-28 16:21:37 +0300

    non block theme switcher

--- src/components/theme-switcher.js
+++ src/components/theme-switcher.js
@@ -1,20 +1,24 @@
-import React from "react"
+import React, {useTransition} from "react"
 import {Switch} from "antd"
 import {useThemeSwitcher} from "react-css-theme-switcher"
 import {currentTheme} from "../services/theme-service"
 
+
 const ThemeSwitcher = () => {
   const {switcher, themes, status} = useThemeSwitcher()
-
+  const [isPending, startTransition] = useTransition()
   const isLight = currentTheme.get() === "light"
 
   const toggleDarkMode = () => {
     isLight ? currentTheme.set("dark") : currentTheme.set("light")
-    switcher({theme: isLight ? themes.light : themes.dark})
+
+    startTransition(() => {
+      switcher({theme: isLight ? themes.light : themes.dark})
+    })
   }
 
   return <Switch
-    loading={status === "loading"}
+    loading={isPending}
     checkedChildren="Light"
     unCheckedChildren="Dark"
     defaultChecked={isLight}
